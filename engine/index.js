794e6a807119591b4d74d0e600946482c0ae55d7
Oleg Talaver <partyskytime@gmail.com>
2020-05-08 08:43:04 +0300

    moved server

--- engine/index.js
+++ engine/index.js
@@ -1,8 +1,50 @@
 const initLib = require("./lib");
+const routeDispatcher = require("./lib/router/router");
 
-module.exports = function initEngine() {
+(function initEngine() {
 	const options = {
 		root: module.parent.path,
 	};
 	initLib(options);
-};
+})();
+
+class Server {
+	constructor() {
+		this.dispatcher = new routeDispatcher();
+	}
+
+	listen(port = null) {
+		const http = require("http");
+		let hostname = "127.0.0.1";
+		port = 8080;
+
+		const server = http.createServer((req, res) => {
+			this.dispatcher.dispatch(req, res);
+			res.statusCode = 200;
+			res.setHeader("Content-Type", "text/plain");
+			res.end("Hello World");
+		});
+
+		server.listen(port, hostname, () => {
+			console.log(`Server running at http://${hostname}:${port}/`);
+		});
+	}
+
+	// TODO delete and replace with auto loading from file
+	addRule(methods, mountingPath, handlers) {
+		this.dispatcher.defineRule(methods, mountingPath, handlers);
+	}
+
+	addRoute(methods, mountingPath, handler) {
+		this.dispatcher.defineRoute(methods, mountingPath, handler);
+	}
+}
+
+module.exports = Server;
+
+// App.use(methods, mp, processor|controller)
+//		.use(...)
+// App.get(handler)
+// 	   .post(handler)
+
+module.exports = Server;
