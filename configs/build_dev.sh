cacd2e9fce8f057ff0ca60c628f706d5c8a7e821
Oleg Talaver <talaveroleg@gmail.com>
2020-06-10 05:38:41 +0100

    configured docker to start front blog react app and nginx proxy server, added configs for the server

--- configs/build_dev.sh
+++ configs/build_dev.sh
@@ -2,15 +2,20 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 
-# go to blog folder
-# cd ../tech_overload_blog 
-# npm run build
-# cp ../tech_overload_blog ../src/tech_overload_blog
-# cp ../admin_panel ../src/admin_panel
+if [ ! -d ../src ]; then
+    mkdir ../src
+fi
 
 if [ ! -e ../src/bluro_cms ]; then
+    echo copy bluro_cms
     cp -r ../bluro_cms ../src/bluro_cms
     cp ./dev_entrypoint.sh ../src/bluro_cms/dev_entrypoint.sh
+fi
+
+if [ ! -e ../src/tech_overload_blog ]; then
+    echo copy tech_overload_blog
+    cp -r ../tech_overload_blog ../src/tech_overload_blog
+    cp ./dev_entrypoint.sh ../src/tech_overload_blog/dev_entrypoint.sh
 fi
 
 docker-compose -f docker-compose.base.yml -f docker-compose.dev.yml up
--- configs/dev_entrypoint.sh
+++ configs/dev_entrypoint.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
 cd /application
+echo Install dependencies
 npm install
+
+echo Start an app
 npm start
